7a1a0281ce7d0613131440cfe96e54986de8fb37
Meghana629 <meghanakatkam01@gmail.com>
2024-11-08 10:58:07 +0530

    Add files via upload

---
+++ if else.js
@@ -0,0 +1,29 @@
+// num=prompt("enter a number")
+// for(i=num;!(i>=-0);i++){
+//     console.log(i)
+// }
+
+// num=prompt("enter a number")
+// for(i=-5;!(i>=0);i++){
+//     console.log(i)
+// }
+
+// num=prompt("enter a number")
+// sum=0
+// if(num>0){
+//     for(i=1;i<num.length;i++)
+//         if(num[i]%2==0)
+//             sum=sum+Number(num[i])
+// }
+// console.log(sum)
+
+
+num=prompt("enter a number")
+sum=0
+if(num>0){
+for(i=0;i<num.length;i++)
+    if(!(num[i]%2==0)){
+        sum=sum+Number(num[i])
+}
+}
+console.log(sum)
